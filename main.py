34f0fb59bf7b4085212b71208d235280668791a7
streethacker <silverbullet7714@126.com>
2016-04-01 15:40:42 +0800

    [FMT] pep8

--- main.py
+++ main.py
@@ -1,115 +1,153 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
-import sys, getopt
+import sys
+import getopt
 import StringIO
 from httphandler import OpenHTTPResource
 from xmlhandler import XMLHandler
 
 URL_HEAD_CONST = "http://flash.weather.com.cn/wmaps/xml/"
 
-PROVINCE_LIST = ["heilongjiang", "jilin", "liaoning", "hainan", "neimenggu", "xinjiang", "xizang", "qinghai", \
-				"ningxia", "gansu", "hebei", "henan", "hubei", "hunan", "shandong", "jiangsu", "anhui", "shanxi" \
-				"sanxi", "sichuan", "yunnan", "guizhou", "zhejiang", "fujian", "jiangxi", "guangdong", "guangxi", \
-				"beijing", "tianjin", "shanghai", "chongqing", "xianggang", "aomen", "taiwan"]
+PROVINCE_LIST = [
+    "heilongjiang",
+    "jilin",
+    "liaoning",
+    "hainan",
+    "neimenggu",
+    "xinjiang",
+    "xizang",
+    "qinghai",
+    "ningxia",
+    "gansu",
+    "hebei",
+    "henan",
+    "hubei",
+    "hunan",
+    "shandong",
+    "jiangsu",
+    "anhui",
+    "shanxi"
+    "sanxi",
+    "sichuan",
+    "yunnan",
+    "guizhou",
+    "zhejiang",
+    "fujian",
+    "jiangxi",
+    "guangdong",
+    "guangxi",
+    "beijing",
+    "tianjin",
+    "shanghai",
+    "chongqing",
+    "xianggang",
+    "aomen",
+    "taiwan"]
 
-class OutOfRangeException(Exception): pass
+
+class OutOfRangeException(Exception):
+    pass
+
 
 def InputUnified(source):
-	"""
-	Unified the input formal of data:
-		already a file or file-like object --> returned directly;
-		a gzip object --> return a StringIO file-like object;
-		a local file path --> return a file object opened by the given path;
-		a url --> return a file-object opened by urlopen() function;
-		a string --> return a StringIO file-linke object.
-	"""
-	if hasattr(source, "read"):
-			if hasattr(source, "headers"):
-					if source.headers.get("Content-Encoding"):
-							import gzip
+    """
+    Unified the input formal of data:
+            already a file or file-like object --> returned directly;
+            a gzip object --> return a StringIO file-like object;
+            a local file path --> return a file object opened by the given path;
+            a url --> return a file-object opened by urlopen() function;
+            a string --> return a StringIO file-linke object.
+    """
+    if hasattr(source, "read"):
+        if hasattr(source, "headers"):
+            if source.headers.get("Content-Encoding"):
+                import gzip
 
-							compressdata = StringIO.StringIO(source.read())
-							gzipper = gzip.GzipFile(fileobj=compressdata)
-							return StringIO.StringIO(gzipper.read())
-					else:
-							return source
+                compressdata = StringIO.StringIO(source.read())
+                gzipper = gzip.GzipFile(fileobj=compressdata)
+                return StringIO.StringIO(gzipper.read())
+            else:
+                return source
 
-	try:
-		return open(source, "r")
-	except IOError, OSError:
-		pass
+    try:
+        return open(source, "r")
+    except IOError as OSError:
+        pass
 
-	try:
-		import urllib
-		return urllib.urlopen(source)
-	except IOError, OSError:
-		pass
+    try:
+        import urllib
+        return urllib.urlopen(source)
+    except IOError as OSError:
+        pass
 
-	return StringIO.StringIO(str(source))
+    return StringIO.StringIO(str(source))
+
 
 def usage():
-	document = \
-	"""
+    document = \
+        """
 	Brief introduction of how to use this module:
 		if you want to get the information of the whole country(main cities):
 				python main.py
 		if you want to get the information of a certain province(main cities of this province):
 				python main.py -F province_name or python main.py --field province_name
 		if you want to get the information of a certain city of a certain province:
-				python main.py -F province_name -c city_name or 
+				python main.py -F province_name -c city_name or
 				python main.py --field province_name --city city_name
 	NOTE: for some specifial fields namely ["xisha", "nansha", "diaoyudao"], you should specific the field as "china":
 				python main.py -F china -c xisha or
 				python main.py --field china --city xisha
 	"""
 
-	print document
+    print document
+
 
 def main(argv):
-	try:
-		opts, args = getopt.getopt(argv, "F:c:h", ["field=", "city=", "help"])
-	except getopt.GetoptError as err:
-		print "Error:", str(err)
-		usage()
-		sys.exit(1)
+    try:
+        opts, args = getopt.getopt(argv, "F:c:h", ["field=", "city=", "help"])
+    except getopt.GetoptError as err:
+        print "Error:", str(err)
+        usage()
+        sys.exit(1)
 
-	opts_dict = {k:v for k, v in opts} #turn (opt, arg) pairs to a dictionary, easy for seeking.
+    # turn (opt, arg) pairs to a dictionary, easy for seeking.
+    opts_dict = {k: v for k, v in opts}
 
-	if opts_dict.has_key("-h") or opts_dict.has_key("--help"):
-			usage()
-	elif opts_dict.has_key("-F") or opts_dict.has_key("--field"):
-			field = opts_dict.get("-F") or opts_dict.get("--field")
-			city = opts_dict.get("-c") or opts_dict.get("--city")
-			if field == "china":
-					target = URL_HEAD_CONST + "china.xml"
-					urldata = OpenHTTPResource(target)
-					source = InputUnified(urldata)
-					parser = XMLHandler(source)
-					return parser.parse(city)
-			elif field in PROVINCE_LIST:
-					target = URL_HEAD_CONST + field + ".xml"
-					urldata = OpenHTTPResource(target)
-					source = InputUnified(urldata)
-					parser = XMLHandler(source)
-					return parser.parse(city)
-			else:
-					raise OutOfRangeException, "Unsupported province of country name."
-	else:
-			target = URL_HEAD_CONST + "china.xml"
-			urldata = OpenHTTPResource(target)
-			source = InputUnified(urldata)
-			parser = XMLHandler(source)
-			return parser.parse()
+    if "-h" in opts_dict or "--help" in opts_dict:
+        usage()
+    elif "-F" in opts_dict or "--field" in opts_dict:
+        field = opts_dict.get("-F") or opts_dict.get("--field")
+        city = opts_dict.get("-c") or opts_dict.get("--city")
+        if field == "china":
+            target = URL_HEAD_CONST + "china.xml"
+            urldata = OpenHTTPResource(target)
+            source = InputUnified(urldata)
+            parser = XMLHandler(source)
+            return parser.parse(city)
+        elif field in PROVINCE_LIST:
+            target = URL_HEAD_CONST + field + ".xml"
+            urldata = OpenHTTPResource(target)
+            source = InputUnified(urldata)
+            parser = XMLHandler(source)
+            return parser.parse(city)
+        else:
+            raise OutOfRangeException("Unsupported province of country name.")
+    else:
+        target = URL_HEAD_CONST + "china.xml"
+        urldata = OpenHTTPResource(target)
+        source = InputUnified(urldata)
+        parser = XMLHandler(source)
+        return parser.parse()
 
 
 if __name__ == "__main__":
-		result = main(sys.argv[1:])		
-		print "城市：".ljust(25), result.get("cityname")
-		print "天气：".ljust(25), result.get("stateDetailed")
-		print "最高气温：".ljust(25), result.get("temHigh")
-		print "最低气温：".ljust(25), result.get("temLow")
-		print "当前气温：".ljust(25), result.get("temNow")
-		print "风力：".ljust(25), result.get("windState")
-		print "湿度：".ljust(25), result.get("humidity")
-		print "发布时间：".ljust(25), result.get("time")
+    result = main(sys.argv[1:])
+    print "城市：".ljust(25), result.get("cityname")
+    print "天气：".ljust(25), result.get("stateDetailed")
+    print "最高气温：".ljust(25), result.get("temHigh")
+    print "最低气温：".ljust(25), result.get("temLow")
+    print "当前气温：".ljust(25), result.get("temNow")
+    print "风力：".ljust(25), result.get("windState")
+    print "湿度：".ljust(25), result.get("humidity")
+    print "发布时间：".ljust(25), result.get("time")
--- xmlhandler.py
+++ xmlhandler.py
@@ -5,101 +5,114 @@ __all__ = ["XMLHandler"]
 
 import xml.etree.ElementTree as ET
 
+
 class XMLHandler:
-	def __init__(self, source):
-		"""
-		Accept a file or file-like object of xml, parse by ET(xml.etree.ElementTree) then
-		get the root Element.
-		"""
-		self.tree = ET.parse(source)
-		self.root = self.tree.getroot()
 
-	def parse(self, pyName=None):
-		"""
-		The parse method plays an role of distributor, selecting the proper parse_xxx method
-		by self-inspection.
-		"""
-		if self.root.tag == "china":
-				parseMethod = getattr(self, "parse_china")
-				return parseMethod(pyName)
-		else:
-				parseMethod = getattr(self, "parse_province")
-				return parseMethod(pyName)
+    def __init__(self, source):
+        """
+        Accept a file or file-like object of xml, parse by ET(xml.etree.ElementTree) then
+        get the root Element.
+        """
+        self.tree = ET.parse(source)
+        self.root = self.tree.getroot()
 
-	def parse_province(self, pyName=None):
-		"""
-		If self.root.tag is not 'china', then this method will be excuted.
-		If parameter pyName is given, a detailed weather report of specific city will be returned as a dictionary:
-			{"cityname":cityname, "stateDetailed":stateDetailed, "temHigh":temHigh, "temLow":temLow, "temNow":temNow, \
-			"windState":windState, "humidity":humidity, "time":time}
+    def parse(self, pyName=None):
+        """
+        The parse method plays an role of distributor, selecting the proper parse_xxx method
+        by self-inspection.
+        """
+        if self.root.tag == "china":
+            parseMethod = getattr(self, "parse_china")
+            return parseMethod(pyName)
+        else:
+            parseMethod = getattr(self, "parse_province")
+            return parseMethod(pyName)
 
-		Otherwise, a brief weather report of all the cities of the province will be returned as a folded dictionary: 
-			{"cityname1":{state of the first city},
-			"cityname2":{state of the second city},
-			...
-			"citynameX":{state of the Xth city}}
-		"""
-		self.status = {}
-		if pyName:
-				for city in self.root.findall("city"):
-						if city.get("pyName") == pyName:
-								self.status["cityname"] = city.get("cityname")
-								self.status["stateDetailed"] = city.get("stateDetailed")
-								self.status["temHigh"] = city.get("tem2")
-								self.status["temLow"] = city.get("tem1")
-								self.status["temNow"] = city.get("temNow")
-								self.status["windState"] = city.get("windState")
-								self.status["humidity"] = city.get("humidity")
-								self.status["time"] = city.get("time")
+    def parse_province(self, pyName=None):
+        """
+        If self.root.tag is not 'china', then this method will be excuted.
+        If parameter pyName is given, a detailed weather report of specific city will be returned as a dictionary:
+                {"cityname":cityname, "stateDetailed":stateDetailed, "temHigh":temHigh, "temLow":temLow, "temNow":temNow, \
+                "windState":windState, "humidity":humidity, "time":time}
 
-								break
+        Otherwise, a brief weather report of all the cities of the province will be returned as a folded dictionary:
+                {"cityname1":{state of the first city},
+                "cityname2":{state of the second city},
+                ...
+                "citynameX":{state of the Xth city}}
+        """
+        self.status = {}
+        if pyName:
+            for city in self.root.findall("city"):
+                if city.get("pyName") == pyName:
+                    self.status["cityname"] = city.get("cityname")
+                    self.status["stateDetailed"] = city.get("stateDetailed")
+                    self.status["temHigh"] = city.get("tem2")
+                    self.status["temLow"] = city.get("tem1")
+                    self.status["temNow"] = city.get("temNow")
+                    self.status["windState"] = city.get("windState")
+                    self.status["humidity"] = city.get("humidity")
+                    self.status["time"] = city.get("time")
 
-				return self.status
-		else:
-				extractMethod = lambda d: {k:v for k, v in d.items() if k in ["stateDetailed", "tem1", "tem2", "windState"]}
-				for city in self.root.findall("city"):
-						self.status[city.get("cityname")] = extractMethod(city.attrib)
+                    break
 
-				return self.status
+            return self.status
+        else:
+            extractMethod = lambda d: {
+                k: v for k, v in d.items() if k in [
+                    "stateDetailed", "tem1", "tem2", "windState"]}
+            for city in self.root.findall("city"):
+                self.status[city.get("cityname")] = extractMethod(city.attrib)
 
-	def parse_china(self, pyName=None):
-		"""
-		If self.root.tag is 'china', this method will be excuted.
-		If pyName is given, it must within the range: ['xisha', 'nansha', 'diaoyudao'], then the weather report of the specific
-		field will be returned as a dictionary:
-			{"cityname":cityname, "stateDetailed":stateDetailed, "temHigh":temHigh, "temLow":temLow, "windState":windState}
+            return self.status
 
-		Otherwise a brief weather report of all the main cities of china will be returned as a folded dictionary:
-			{"cityname1":{"quName1":quName1, the state of the city},
-			"cityname2":{"quName2":quName2, the state of the city},
-			...
-			"citynameX":{"quNameX":quNameX, the state of the city}}
-		"""
-		self.status = {}
+    def parse_china(self, pyName=None):
+        """
+        If self.root.tag is 'china', this method will be excuted.
+        If pyName is given, it must within the range: ['xisha', 'nansha', 'diaoyudao'], then the weather report of the specific
+        field will be returned as a dictionary:
+                {"cityname":cityname, "stateDetailed":stateDetailed, "temHigh":temHigh, "temLow":temLow, "windState":windState}
 
-		extractMethod = lambda d: {k:v for k, v in d.items() if k in ["quName","stateDetailed", "tem1", "tem2", "windState"]}
-		if pyName in ["xisha", "nansha", "diaoyudao"]:
-				for city in self.root.findall("city"):
-						if city.get("pyName") == pyName:
-								self.status["cityname"] = city.get("cityname")
-								self.status["stateDetailed"] = city.get("stateDetailed")
-								self.status["temLow"] = city.get("tem2")
-								self.status["temHigh"] = city.get("tem1")
-								self.status["windState"] = city.get("windState")
+        Otherwise a brief weather report of all the main cities of china will be returned as a folded dictionary:
+                {"cityname1":{"quName1":quName1, the state of the city},
+                "cityname2":{"quName2":quName2, the state of the city},
+                ...
+                "citynameX":{"quNameX":quNameX, the state of the city}}
+        """
+        self.status = {}
 
-								break
+        extractMethod = lambda d: {
+            k: v for k,
+            v in d.items() if k in [
+                "quName",
+                "stateDetailed",
+                "tem1",
+                "tem2",
+                "windState"]}
+        if pyName in ["xisha", "nansha", "diaoyudao"]:
+            for city in self.root.findall("city"):
+                if city.get("pyName") == pyName:
+                    self.status["cityname"] = city.get("cityname")
+                    self.status["stateDetailed"] = city.get("stateDetailed")
+                    self.status["temLow"] = city.get("tem2")
+                    self.status["temHigh"] = city.get("tem1")
+                    self.status["windState"] = city.get("windState")
 
-				return self.status
-		else:
-				for city in self.root.findall("city"):
-						self.status[city.get("cityname")] = extractMethod(city.attrib)
+                    break
 
-				return self.status
+            return self.status
+        else:
+            for city in self.root.findall("city"):
+                self.status[city.get("cityname")] = extractMethod(city.attrib)
+
+            return self.status
 
 if __name__ == "__main__":
-		import urllib, StringIO, pprint
-		data = urllib.urlopen("http://flash.weather.com.cn/wmaps/xml/jiangsu.xml")
-		source = StringIO.StringIO(data.read())
-		parser = XMLHandler(source)
-		result = parser.parse()
-		pprint.pprint(result)
+    import urllib
+    import StringIO
+    import pprint
+    data = urllib.urlopen("http://flash.weather.com.cn/wmaps/xml/jiangsu.xml")
+    source = StringIO.StringIO(data.read())
+    parser = XMLHandler(source)
+    result = parser.parse()
+    pprint.pprint(result)
